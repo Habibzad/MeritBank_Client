95fb35e1820c2e5650f981cd51e9389bddd331e1
Gabriel Gualteros <Roblox4@gmail.com>
2021-06-03 14:04:05 -0400

    add welcome user component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,22 +1,58 @@
 import React from 'react'
 
-constructor() {
-    super()
-    this.state = {
-        message: 'Welcome User'
-    }
-}
+/* globals Chart:false, feather:false */
 
-const WelcomeUser = () => {
-    return (
-        <div>
-            <h1>
-                {this.state.message}
-            </h1>
-            <button type="button" class="btn btn-danger">Sign out</button>
-        //Removed
-        </div>
-    )
-}
+(function () {
+    'use strict'
+  
+    feather.replace()
+  
+    // Graphs
+    var ctx = document.getElementById('myChart')
+    // eslint-disable-next-line no-unused-vars
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: [
+          'Sunday',
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday'
+        ],
+        datasets: [{
+          data: [
+            15339,
+            21345,
+            18483,
+            24003,
+            23489,
+            24092,
+            12034
+          ],
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: '#007bff',
+          borderWidth: 4,
+          pointBackgroundColor: '#007bff'
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        }
+      }
+    })
+  })()
+  
 
 export default WelcomeUser
