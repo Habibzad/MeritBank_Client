79f562b9de4689603119b9e0d4267c38bdcd0fd8
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-06-18 14:32:11 -0400

    close account

--- src/components/accounts/Accounts.js
+++ src/components/accounts/Accounts.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { AuthorizationContext } from '../../AuthorizationContext'
 import { Redirect, useHistory } from 'react-router-dom'
-import { Table } from 'react-bootstrap'
+import { Table, Alert } from 'react-bootstrap'
 
 function Accounts() {
     const [accounts, setAccounts] = useState([]);
-    const [store] = useContext(AuthorizationContext)
+    const [store, setStore] = useContext(AuthorizationContext)
     const isLoggedIn = store.isLoggedIn;
     const role = store.role;
     const jwt = store.jwt
     const history = useHistory();
+    const successMessage = store.successMessage;
 
     useEffect(() => {
         displayAccounts();
@@ -36,8 +37,25 @@ function Accounts() {
             .catch(error => console.log('error', error));
     }
 
-    const updateAccount = (id) => {
-        console.log(id)
+    const closeAccount = (id) => {
+        const myHeaders = {
+            "Authorization": "Bearer " + jwt,
+            "Content-Type": "application/json"
+        }
+
+        var requestOptions = {
+            method: 'PUT',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        fetch(`http://localhost:8080/api/close-account/${id}`, requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                console.log(result)
+                displayAccounts();
+            })
+            .catch(error => console.log('error', error));
     }
 
     const deleteAccount = (id) => {
@@ -48,8 +66,14 @@ function Accounts() {
         return <Redirect to="/user" />
     }
 
+    if (successMessage !== '') {
+        setTimeout(() => setStore({ ...store, successMessage: '' }), 2000)
+    }
+
     return (
         <div className="container">
+            {successMessage &&
+                <Alert className="alert" variant='success'>{successMessage}</Alert>}
             <h3 className="component-header">Accounts</h3>
             <div className="">
                 <Table style={{ backgroundColor: 'white', textAlign: 'center' }} className="table table-striped table-bordered">
@@ -68,6 +92,7 @@ function Accounts() {
                             <th>Opening Date</th>
                             <th>Account Type</th>
                             <th>Status</th>
+                            <th>Close Account</th>
                         </tr>
                     </thead>
                     <tbody >
@@ -75,7 +100,7 @@ function Accounts() {
                             accounts.map(account =>
                                 <tr key={account.accountNumber}>
                                     <td>
-                                        <i className="fas fa-pencil-alt text-warning" onClick={() => updateAccount(account.accountNumber)} style={{ marginRight: '30px', cursor: 'pointer' }}></i>
+                                        {/* <i className="fas fa-pencil-alt text-warning" onClick={() => updateAccount(account.accountNumber)} style={{ marginRight: '30px', cursor: 'pointer' }}></i> */}
                                         <i className="fas fa-user-slash text-danger" style={{ cursor: 'pointer' }}
                                             onClick={() => {
                                                 if (window.confirm(`Are you sure you want to delete account: ${account.accountNumber}`)) {
@@ -90,6 +115,11 @@ function Accounts() {
                                     <td>{account.openingDate}</td>
                                     <td>{account.accountType}</td>
                                     <td>{account.status}</td>
+                                    <td style={{ color: 'blue', cursor: 'pointer' }} onClick={() => {
+                                        if (window.confirm(`Are you sure you want to close account number: ${account.accountNumber}`)) {
+                                            closeAccount(account.accountNumber);
+                                        }
+                                    }}>Close Account</td>
                                 </tr>)
                         }
                     </tbody>
